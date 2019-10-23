93ed9da38b4fd3b255a38853808b9420b01dc8dc
silvasobrinho <giovane.sobrinho@gmail.com>
2019-10-23 13:24:39 -0300

    busca

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const apiCerva = "https://api.punkapi.com/v2/beers";
+
+$.getJSON(aoiCerva, function(data){
+
+
+let buscar = data.filter(document.getElementById("search-input").value  )
+
+
+
+
+
+
+
+}
