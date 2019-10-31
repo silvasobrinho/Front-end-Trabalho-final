bef11d3385558504011ca7d98d340357132c5565
Giovaen da Silva Sobrinho <giovane.sobrinho@gmail.com>
2019-10-31 11:21:44 -0300

    update busca avancada

--- buscaavancada.js
+++ buscaavancada.js
@@ -1,52 +1,81 @@
-function escreveTela(cervejas){
-	this.$('#bCerveja').html('')
-	console.log(cervejas);
-	    // NAO funcionou  $.each(cervejas, function(element){
-		//for(let element of cervejas){
-		//for(let element in cervejas){
-		//cervejas.forEach(function(item, index, array){
-		cervejas.forEach(element => {
-		
-		console.log("200 ok entrei");	
-        $('#bCerveja').append(
-            `}
-			<div class="col-lg-4 col-md-6 col-sm-12 mt-4">
-			<div class="card " style="width: 18rem;">
-		   
-				<img class="card-img-top smallimg" src="${element.image_url}">
-				<div class="card-body ">
-					<h5 class="card-title">${element.name}</h5>
-					<p class="card-text">${element.description}</p>
-			  
-				</div>
-			</div>
-		</div>`
-		)
-		console.log("200 ok 2");
-	});
-	
+var todascervas = [];
+
+const urls = ['https://api.punkapi.com/v2/beers?page=1&per_page=80',
+'https://api.punkapi.com/v2/beers?page=2&per_page=80',
+'https://api.punkapi.com/v2/beers?page=3&per_page=80',
+'https://api.punkapi.com/v2/beers?page=4&per_page=80',
+'https://api.punkapi.com/v2/beers?page=5&per_page=80'];
+
+vainaAPI(urls).then(response =>{
+	console.log ("executando!")
+})
+.catch(error => {
+	console.log('error!');
+	console.log(error);
+});
+
+async function vainaAPI(urls){
+	for(let url of urls){
+		console.log(todascervas)
+		const response = await fetch(url);
+		const lcervejas = await response.json();
+		todascervas.push(...lcervejas)
+	}
+	console.log(todascervas)
 }
 
+console.log(todascervas)
 
 
-var todasCervejas = [];
-   	   for(var i=1; i<6; i++) {
-      fetch(`https://api.punkapi.com/v2/beers?page=${i}&per_page=80`)
-        .then(function(resp) {
-          return resp.json();
-        })
-        .then(function(data) {
-			//Esse trem deu trabalho pra ficar colocando um dentro do outro NÂO ESQUECER PORRA!
-    	todasCervejas.push(...data)
-		});
-	}
+// function escreveTela(cervejas){
+// 	this.$('#bCerveja').html('')
+// 	console.log(cervejas);
+// 	    // NAO funcionou  $.each(cervejas, function(element){
+// 		//for(let element of cervejas){
+// 		//for(let element in cervejas){
+// 		//cervejas.forEach(function(item, index, array){
+// 		cervejas.forEach(element => {
+		
+// 		console.log("200 ok entrei");	
+//         $('#bCerveja').append(
+//             `}
+// 			<div class="col-lg-4 col-md-6 col-sm-12 mt-4">
+// 			<div class="card " style="width: 18rem;">
+		   
+// 				<img class="card-img-top smallimg" src="${element.image_url}">
+// 				<div class="card-body ">
+// 					<h5 class="card-title">${element.name}</h5>
+// 					<p class="card-text">${element.description}</p>
+			  
+// 				</div>
+// 			</div>
+// 		</div>`
+// 		)
+// 		console.log("200 ok 2");
+// 	});
 	
-	escreveTela(todasCervejas);
+// }
 
 
-	for(let [key, value] of Object.entries(todasCervejas)){
-		console.log('key',key , value)
-	}
+
+// var todasCervejas = [];
+//    	   for(var i=1; i<6; i++) {
+//       fetch(`https://api.punkapi.com/v2/beers?page=${i}&per_page=80`)
+//         .then(function(resp) {
+//           return resp.json();
+//         })
+//         .then(function(data) {
+// 			//Esse trem deu trabalho pra ficar colocando um dentro do outro NÂO ESQUECER PORRA!
+//     	todasCervejas.push(...data)
+// 		});
+// 	}
+	
+// 	escreveTela(todasCervejas);
+
+
+// 	for(let [key, value] of Object.entries(todasCervejas)){
+// 		console.log('key',key , value)
+// 	}
 	
 /* Não funcionou tbm mesmo erro
 var aaa = [];
